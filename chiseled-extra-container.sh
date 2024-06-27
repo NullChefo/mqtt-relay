b82f656d237957a198011ada4546fd5b26cca1cb
nullchefo <stefan@nullchefo.com>
2024-06-27 14:35:56 +0300

    add the x64 and arm64 docker

--- chiseled-extra-container.sh
+++ chiseled-extra-container.sh
@@ -1,3 +1,10 @@
 ﻿#!/bin/bash
 
-dotnet publish -c Release /p:PublishTrimmed=true -r linux-x64 /p:PublishSelfContained=true /p:ContainerFamily=jammy-chiseled-extra /p:ContainerRepository=mqtt-relay-chiseled /t:PublishContainer
+dotnet publish -c Release -r linux-x64 /p:PublishTrimmed=true /p:PublishSelfContained=true /p:ContainerFamily=jammy-chiseled-extra /p:ContainerRepository=mqtt-relay-chiseled /t:PublishContainer
+
+
+
+dotnet publish -c Release -r linux-arm64 /p:PublishTrimmed=true /p:PublishSelfContained=true /p:ContainerFamily=jammy-chiseled /p:ContainerRepository=mqtt-relay-chiseled /t:PublishContainer
+
+
+docker run  mqtt-relay-chiseled:latest
